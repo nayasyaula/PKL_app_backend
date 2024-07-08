905ebe467e52f17d264239ba5e444f62e4817135
nayasyaula <[200~nayasyaulamawarni@smkwikrama.sch.idgit config --global user.email nayasyaulamawarni@smkwikrama.sch.id>
2024-07-08 20:20:10 +0700

    baru lagi

--- routes/web.php
+++ routes/web.php
@@ -13,17 +13,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
-
-// Route::resource('todolist', ToDoListController::class);
-Route::get('/ToDoList/index', [ToDoListController::class, 'index'])->name('ToDoList.index');
-Route::get('/todolist/create', [ToDoListController::class, 'create'])->name('ToDoList.create');
-Route::post('/todolist/store', [ToDoListController::class, 'store'])->name('ToDoList.store');
-Route::get('/todolist/{todolist}/edit', [ToDoListController::class, 'edit'])->name('ToDoList.edit');
-Route::put('/todolist/{todolist}/update', [ToDoListController::class, 'update'])->name('ToDoList.update');
-Route::delete('/todolist/{id}/delete', [ToDoListController::class, 'destroy'])->name('ToDoList.destroy');
-Route::put('/todolist/{id}/updateStatus', [ToDoListController::class, 'updateStatus'])->name('ToDoList.updateStatus');
-
 Route::middleware(['auth'])->group(function () {
     Route::get('/home', [HomeController::class, 'index'])->name('home');
     Route::get('/pages', [AttendanceModelController::class, 'index'])->name('pages');
@@ -32,6 +21,15 @@ Route::middleware(['auth'])->group(function () {
 
     Route::get('/user', [UserController::class, 'index'])->name('user');
     Route::get('/user/show/{id}', [UserController::class, 'show'])->name('show');
+
+    // Route::resource('todolist', ToDoListController::class);
+    Route::get('/ToDoList/index', [ToDoListController::class, 'index'])->name('ToDoList.index');
+    Route::get('/todolist/create', [ToDoListController::class, 'create'])->name('ToDoList.create');
+    Route::post('/todolist/store', [ToDoListController::class, 'store'])->name('ToDoList.store');
+    Route::get('/todolist/{todolist}/edit', [ToDoListController::class, 'edit'])->name('ToDoList.edit');
+    Route::put('/todolist/{todolist}/update', [ToDoListController::class, 'update'])->name('ToDoList.update');
+    Route::delete('/todolist/{id}/delete', [ToDoListController::class, 'destroy'])->name('ToDoList.destroy');
+    Route::put('/todolist/{id}/updateStatus', [ToDoListController::class, 'updateStatus'])->name('ToDoList.updateStatus');
 
     // Route::middleware(['checkUserRole'])->group(function () {
     //     // Routes yang membutuhkan pengecekan role
