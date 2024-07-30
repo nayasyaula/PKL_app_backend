22355e17d0c62bcdcad5230eaf5e617376c2659a
nayasyaula <[200~nayasyaulamawarni@smkwikrama.sch.idgit config --global user.email nayasyaulamawarni@smkwikrama.sch.id>
2024-07-30 09:27:23 +0700

    gileee

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
-use App\Http\Controllers\UserController;
-use Illuminate\Routing\Route;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::get('/users', [UserController::class, 'index']);
-Route::post('/login', [UserController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'register']);
